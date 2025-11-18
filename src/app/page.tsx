2d598dfd68f8838b2145acd4d36b4634f9e461c1
Alexandre Cruv <alefeufei@gmail.com>
2025-11-18 01:52:44 -0300

    terminei a fase um com botões, cards e inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus, List, Check, CircleX } from "lucide-react";
+
 const Home = () => {
   return (
-    <div>Odair</div>
-  )
-}
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-2">
+        <div className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button variant="outline" className="cursor-pointer">
+            <Plus /> Cadastrar
+          </Button>
+        </div>
 
-export default Home
+        <Separator />
+
+        <div>
+          <Badge>
+            <List />
+            todas
+          </Badge>
+          <Badge>
+            <CircleX />
+            Não Finalizadas
+          </Badge>
+          <Badge>
+            <Check />
+            Concluidas
+          </Badge>
+        </div>
+      </Card>
+    </main>
+  );
+};
+
+export default Home;
